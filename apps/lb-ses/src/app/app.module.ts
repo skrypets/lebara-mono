bbb6d83da15ceee3b4615c82203026ad635d8a70
Mykola Skrypets <mykola.skrypets@lebara.com>
2019-11-13 22:39:32 +0200

    amplify auth components

--- apps/lb-ses/src/app/app.module.ts
+++ apps/lb-ses/src/app/app.module.ts
@@ -2,11 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { AwsAmplifyModule } from '@lebara-mono/aws-amplify';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AwsAmplifyModule],
+  declarations: [AppComponent, SignUpComponent, SignInComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AwsAmplifyModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ apps/lb-ses/src/app/auth/auth.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+import Auth from '@aws-amplify/auth';
+import { Router } from '@angular/router';
+import { Hub } from '@aws-amplify/core';
+
+@Component({
+  selector: 'lebara-mono-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss']
+})
+export class AuthComponent implements OnInit {
+  constructor(private _router: Router, private _zone: NgZone) {}
+
+  ngOnInit() {
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+          this._zone.run(() => {
+            this._router.navigate(['/']);
+          });
+          break;
+        case 'signOut':
+          this._zone.run(() => {
+            this._router.navigate(['/auth/signin']);
+          });
+          break;
+      }
+    });
+    Auth.currentAuthenticatedUser()
+      .then(() => {
+        this._router.navigate(['auth/profile']);
+      })
+      .catch(() => {});
+  }
+}
